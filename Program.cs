e759b670f21b53f8312dcc2e15536fbee020fb65
nicolasxz13 <nicolasxz13@gmail.com>
2023-09-30 01:56:23 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿List<object> listaobject = new List<object>();
+
+listaobject.Add(7);
+listaobject.Add(28);
+listaobject.Add(-1);
+listaobject.Add(true);
+listaobject.Add("chair");
+
+foreach(var result in listaobject){
+    Console.WriteLine(result);
+}
+int sumTemp = 0;
+foreach(var result in listaobject){
+    if(result is int){
+        sumTemp += (int)result;
+    }
+}
+Console.WriteLine(sumTemp);
